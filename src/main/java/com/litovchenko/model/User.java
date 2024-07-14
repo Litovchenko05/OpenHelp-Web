89085642c359326f644ef50e7adc56cb7dbb6238
Alexander <litovchenko5108@gmail.com>
2024-07-14 13:09:50 -0300

    update User constructor

--- src/main/java/com/litovchenko/model/User.java
+++ src/main/java/com/litovchenko/model/User.java
@@ -1,21 +1,14 @@
 package com.litovchenko.model;
 
+import lombok.Getter;
+
 public class User {
-    private int id;
     private String username;
     private String password;
     private String email;
     private String name;
 
     //Constructors
-    public User (int id, String username, String password, String email, String name){
-        this.id = id;
-        this.username = username;
-        this.password = password;
-        this.email = email;
-        this.name = name;
-    }
-
     public User (String username, String password, String email, String name){
         this.username = username;
         this.password = password;
@@ -23,49 +16,33 @@ public class User {
         this.name = name;
     }
 
-    //getters and setters
-    //Id
-    public int getId(){
-        return id;
-    }
-    public void setId(int id){
-        this.id = id;
-    }
-
-    //Username
-    public String getUsername(){
-        return username;
-    }
     public void setUsername(String username){
         this.username = username;
     }
-
-    //Password
-    public String getPassword(){
-        return password;
+    public String getUsername() {
+        return username;
     }
     public void setPassword(String password){
         this.password = password;
     }
-
-    //Email
-    public String getEmail(){
-        return email;
+    public String getPassword() {
+        return password;
     }
     public void setEmail(String email){
         this.email = email;
     }
-
-    //Name
-    public String getName(){
-        return name;
+    public String getEmail() {
+        return email;
     }
     public void setName(String name){
         this.name = name;
     }
+    public String getName() {
+        return name;
+    }
 
     @Override
     public String toString() {
-        return "User [id= " + id + ", username= " + username + ", password= " + password + ", email= " + email + ", name= " + name + "]";
+        return "[username= " + username + ", password= " + password + ", email= " + email + ", name= " + name + "]";
     }
 }
