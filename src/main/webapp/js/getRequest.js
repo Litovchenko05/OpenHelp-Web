7406e5f68ff62e33ba05bb68fd0338afebbb0ad8
Alexander <litovchenko5108@gmail.com>
2024-07-14 13:09:07 -0300

    update getRequest.js file

--- src/main/webapp/js/getRequest.js
+++ src/main/webapp/js/getRequest.js
@@ -2,26 +2,34 @@ console.log("Enter to GetRequest");
 document.addEventListener("DOMContentLoaded", function() {
 
     const usersTable = document.getElementById("users");
-    const users = [];
+    const usersList = [];
 
     function loadUsersList() {
         console.log("Enter to loadUsersList");
         fetch("/OpenHelp-Web/users?action=getAll")
-            .then(response => response.json())
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error(`Network response was not ok: ${response.status} - ${response.statusText}`);
+                }
+                return response.json();
+            })
             .then(data => {
-                console.log("data : "+data);
-                data.forEach(user =>{
-                    users.push(user);
+                console.log("data : ", data);
+                if (Array.isArray(data)){
+                data.forEach(users =>{
+                    usersList.push(users);
                     usersTable.innerHTML += `
             <tr>
-              <td>${user.username}</td>
-              <td>${user.password}</td>
-              <td>${user.email}</td>
-              <td>${user.name}</td>
+              <td>${users.username}</td>
+              <td>${users.password}</td>
+              <td>${users.email}</td>
+              <td>${users.name}</td>
             </tr>
           `
                 });
-                let table = new DataTable("#users");
+                } else {
+                    console.error("Invalid data format:", data);
+                }
             })
     .catch(error => console.error("Error fetching data: ", error));
     }
