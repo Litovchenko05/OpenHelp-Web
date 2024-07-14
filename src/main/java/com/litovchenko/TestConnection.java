e6fd1ea5039f396e2c5813b729c2a78dfba1916a
Alexander Litovchenko <150480809+Litovchenko05@users.noreply.github.com>
2024-07-14 14:58:37 -0300

    Update TestConnection.java

--- src/main/java/com/litovchenko/TestConnection.java
+++ src/main/java/com/litovchenko/TestConnection.java
@@ -8,14 +8,10 @@ import java.util.List;
 
 public class TestConnection {
     public static void main(String[] args) {
-        //List<User> usersLista = UserDAO.select();
-        //System.out.println(usersLista);
-
+       
         List<User> usersList = new ArrayList<>();
         usersList = UserDAO.select();
         System.out.println(usersList);
-        //User user = new User("MarioLopez", "1512523", "maritoo@gmail.com", "Mario Lopez");
-        //System.out.println(UserDAO.insert(user));
     }
 }
 
