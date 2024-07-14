f008df6b4e6f9b91ff521d83ffc71faa0a848346
Alexander <litovchenko5108@gmail.com>
2024-07-14 13:09:30 -0300

    fix problems with the servlet

--- src/main/java/com/litovchenko/controller/UserServletController.java
+++ src/main/java/com/litovchenko/controller/UserServletController.java
@@ -23,58 +23,52 @@ import org.slf4j.LoggerFactory;
 @WebServlet("/users")
 public class UserServletController extends HttpServlet{
     static Logger logger = LoggerFactory.getLogger(UserServletController.class);
-    List<User> usersList = new ArrayList<>();
     ObjectMapper mapper = new ObjectMapper();
+    List<User> usersList = new ArrayList<>();
 
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
         String route = req.getParameter("action");
         logger.info("route : " + route);
-        switch (route) {
-            case "getAll"-> {
-                res.setContentType("application/json; charset=UTF-8");
-                usersList = UserDAO.select();
-                logger.info("Inside of getAll : " + usersList.size());
-                //transform to json and send it
-                mapper.writeValue(res.getWriter(), usersList);
-                logger.info(mapper.toString());
-            }
-
-            default -> {
-                System.out.println("Invalid parameter");
-            }
+        if (route.equals("getAll")) {
+            handleGetAll(req, res);
+        } else {
+            logger.info("Invalid parameter");
         }
+        }
+    // I use a handle instead of writing the code inside the case
+    private void handleGetAll(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        res.setContentType("application/json; charset=UTF-8");
+        usersList = UserDAO.select();
+        mapper.writeValue(res.getWriter(), usersList);
+        logger.info(mapper.toString());
     }
-
     //In "doPost" we send the form data to Java, then we insert into the database
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-        //String route = req.getParameter("action");
-        String route = "add";
+        String route = req.getParameter("action");
         logger.info("In doPost()");
         logger.info("route : " + route);
         logger.info("req : " + req);
         logger.info("req : " + req.getParameter("name"));
-        switch(route) {
-            case "add"-> {
-                String username = req.getParameter("username");
-                String password = req.getParameter("password");
-                String email = req.getParameter("email");
-                String name = req.getParameter("name");
-                logger.info(username,password,email,name);
+        if (route.equals("add")) {
+            String username = req.getParameter("username");
+            String password = req.getParameter("password");
+            String email = req.getParameter("email");
+            String name = req.getParameter("name");
+            logger.info(username, password, email, name);
 
-                User newUser = new User(username, password, email, name);
+            User newUser = new User(username, password, email, name);
 
-                //Hardcode an User
-                User hardUser = new User("Peposo", "cumbiapeposa1234", "pepoeventos@gmail.com", "El Pepo");
-                UserDAO.insert(hardUser);
+            //Hardcode an User
+            User hardUser = new User("Peposo", "cumbiapeposa1234", "pepoeventos@gmail.com", "El Pepo");
+            UserDAO.insert(hardUser);
 
-                res.setContentType("application/json; charset=UTF-8");
-                Map<String, String> response = new HashMap();
-                response.put("message", "User saved successfully!!!");
-                mapper.writeValue(res.getWriter(), response);
-            }
+            res.setContentType("application/json; charset=UTF-8");
+            Map<String, String> response = new HashMap();
+            response.put("message", "User saved successfully!!!");
+            mapper.writeValue(res.getWriter(), response);
         }
     }
 }
